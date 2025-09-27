38567fed605a3fd4043409ea74d661c15f044106
Zachary Iverson <zach_iverson@trimble.com>
2025-09-27 15:07:24 -0700

    Initial migrations and ef core

--- Program.cs
+++ Program.cs
@@ -3,8 +3,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 builder.Services.AddDbContext<TaskManagementApi.Database.TaskManagementContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
